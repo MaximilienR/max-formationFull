6923291cc1d57d593ed3043bfa9de6c46bace20a
maximilien <ro.maximilien@gmail.com>
2025-07-15 11:21:35 +0200

    change send reset

--- maxformationBack/email/email.js
+++ maxformationBack/email/email.js
@@ -28,7 +28,7 @@ const sendReset = async (email, token) => {
     html: `
       <p>Vous avez demandé la réinitialisation de votre mot de passe.</p>
       <p>Cliquez sur le lien ci-dessous pour réinitialiser votre mot de passe :</p>
-      <a href="http://localhost:5173/reset?token=${token}">Réinitialiser mon mot de passe</a>
+      <a href="${process.env.FRONT}/reset?token=${token}">Réinitialiser mon mot de passe</a>
     `,
   };
 
