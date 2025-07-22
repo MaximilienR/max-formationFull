4cfc439308c78dc92f1de497ddfc667bc3ad8f30
maximilien <ro.maximilien@gmail.com>
2025-07-22 09:23:51 +0200

    update index

--- maxformationBack/index.js
+++ maxformationBack/index.js
@@ -28,13 +28,15 @@ mongoose
   .catch((e) => console.error(e));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../max-formation/dist")));
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../max-formation/dist", "index.html"));
   });
 }
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
