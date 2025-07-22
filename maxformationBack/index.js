f83885dd82a371e71cdbdf1848cd48c83661958c
maximilien <ro.maximilien@gmail.com>
2025-07-22 08:39:15 +0200

    update index.js

--- maxformationBack/index.js
+++ maxformationBack/index.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const allowedOrigin = process.env.FRONT;
+const path = require("path");
 
+const allowedOrigin = process.env.FRONT;
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -16,17 +18,23 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-const route = require("./routes");
 
+const route = require("./routes");
 app.use(route);
 
 mongoose
   .connect(process.env.MOGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((e) => console.error(e));
 
-app.listen(5000);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//localhost:3000/
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
