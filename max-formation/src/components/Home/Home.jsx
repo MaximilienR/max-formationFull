9fb2e726cb20837cc7f4560aa6c9629eca12c3ad
maximilien <ro.maximilien@gmail.com>
2025-07-29 14:01:20 +0200

    change ortographe

--- max-formation/src/components/Home/Home.jsx
+++ max-formation/src/components/Home/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
             </ul>
           </section>
           <section className="w-full p-4 md:w-1/2 xl:w-1/2">
-            <h2 className="mb-2 text-3xl font-bold">Coatching personnalisé</h2>
+            <h2 className="mb-2 text-3xl font-bold">Coaching personnalisé</h2>
             <Link to="/Coatch">
               <button className="px-4 py-2 font-bold text-white bg-yellow-400 rounded hover:bg-yellow-500">
                 Découvrir le coaching
